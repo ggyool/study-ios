0d6822addd8c3ed3362c569d9a73409f395220ef
seunghyun <ggyool@naver.com>
2020-09-01 19:21:52 +0900

    -

---
+++ MyAlbum/MyAlbum/ListToolBar.swift
@@ -0,0 +1,15 @@
+//
+//  ListToolBar.swift
+//  MyAlbum
+//
+//  Created by ggyool on 2020/09/01.
+//  Copyright © 2020 ggyool. All rights reserved.
+//
+
+import UIKit
+
+class ListToolBar: UIToolbar {
+    @IBOutlet weak var trashButton: UIBarButtonItem!
+    @IBOutlet weak var orderStateButton: UIBarButtonItem!
+    @IBOutlet weak var activityButton: UIBarButtonItem!
+}
--- MyAlbum/MyAlbum/ListViewController.swift
+++ MyAlbum/MyAlbum/ListViewController.swift
@@ -9,19 +9,27 @@
 import UIKit
 import Photos
 
-class ListViewController: UIViewController, UICollectionViewDataSource {
+class ListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, PHPhotoLibraryChangeObserver {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var toolBar: ListToolBar!
     
     let cellIdentifier: String = "cell"
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
+
     var collection: PHAssetCollection?
     var fetchResult: PHFetchResult<PHAsset>!
-    let imageManager: PHCachingImageManager = PHCachingImageManager()
+    var selectButton: UIBarButtonItem!
+    var cancelButton: UIBarButtonItem!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initCollectionView()
+        initNavaigationBar()
+        initToolBar()
         requestAsset()
+        PHPhotoLibrary.shared().register(self)
     }
     
     func initCollectionView() {
@@ -33,7 +41,55 @@ class ListViewController: UIViewController, UICollectionViewDataSource {
         flowLayout.minimumLineSpacing = distance // 줄 간의 최소 거리
         flowLayout.itemSize = CGSize(width: (criteria-2*distance)/3, height: (criteria-2*distance)/3)
         collectionView.collectionViewLayout = flowLayout
+        
+        collectionView.allowsSelection = false
+        collectionView.allowsMultipleSelection = false
     }
+    
+    func initNavaigationBar() {
+        self.navigationItem.title = self.collection?.localizedTitle
+        selectButton = UIBarButtonItem(title: "선택", style: .plain, target: self, action: #selector(self.touchUpSelectButton(_:)))
+        cancelButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(self.touchUpCancelButton(_:)))
+        self.navigationItem.rightBarButtonItem = selectButton
+    }
+    
+    func initToolBar() {
+        disableBarButton(toolBar.activityButton)
+        enableBarButton(toolBar.orderStateButton)
+        disableBarButton(toolBar.trashButton)
+    }
+    
+    func disableBarButton(_ button: UIBarButtonItem) {
+        button.tintColor = .lightGray
+        button.isEnabled = false
+    }
+    
+    func enableBarButton(_ button: UIBarButtonItem) {
+        // default tint color 알아낼 방법이 있는지?
+        button.tintColor = toolBar.orderStateButton.tintColor
+        button.isEnabled = true
+    }
+    
+    @objc func touchUpSelectButton(_ sender: UIBarButtonItem) {
+        self.navigationItem.hidesBackButton = true
+        self.navigationItem.title = "항목 선택"
+        self.navigationItem.rightBarButtonItem = cancelButton
+        collectionView.allowsSelection = true
+        collectionView.allowsMultipleSelection = true
+        enableBarButton(toolBar.activityButton)
+        enableBarButton(toolBar.trashButton)
+    }
+    
+    @objc func touchUpCancelButton(_ sender: UIBarButtonItem) {
+        self.navigationItem.hidesBackButton = false
+        self.navigationItem.title = self.collection?.localizedTitle
+        self.navigationItem.rightBarButtonItem = selectButton
+        collectionView.allowsSelection = false
+        collectionView.allowsMultipleSelection = false
+        disableBarButton(toolBar.activityButton)
+        disableBarButton(toolBar.trashButton)
+    }
+
     
     func requestAsset() {
         if collection?.localizedTitle == "Recents" {
@@ -45,6 +101,9 @@ class ListViewController: UIViewController, UICollectionViewDataSource {
             fetchResult = PHAsset.fetchAssets(in: collection!, options: fetchOptions)
         }
     }
+    
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return fetchResult.count
@@ -71,4 +130,25 @@ class ListViewController: UIViewController, UICollectionViewDataSource {
     }
     
 
+    // UICollectionViewDelegate methods
+    
+    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return collectionView.allowsSelection && collectionView.allowsMultipleSelection
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("select " + "\(indexPath.item)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, shouldDeselectItemAt indexPath: IndexPath) -> Bool {
+        print("deSelect " + "\(indexPath.item)")
+        return false
+        
+    }
+    
+    
+    // PHPhotoLibraryChangeObserver methods
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        
+    }
 }
